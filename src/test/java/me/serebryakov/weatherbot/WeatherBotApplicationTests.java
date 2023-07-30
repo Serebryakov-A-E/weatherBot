4dab70bc5683dd1fc12b72b905385e6686a1f1ed
Serebryakov-A-E <117353355+Serebryakov-A-E@users.noreply.github.com>
2023-07-30 13:09:27 +0500

    Initial commit

---
+++ src/test/java/me/serebryakov/weatherbot/WeatherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.serebryakov.weatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
